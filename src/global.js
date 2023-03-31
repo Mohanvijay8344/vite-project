1a0c758f8d280619aba65e663c3823c589111988
Mohanraj <Mohanvijay8344@gmail.com>
2023-03-31 09:29:26 +0530

    Global Update

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 // export const API = "https://63d75fbcafbba6b7c93beb74.mockapi.io";
-// export const API = "https://movies-backend-deploy.vercel.app/";
-export const API = "http://localhost:3005";
+export const API = "https://movies-backend-deploy.vercel.app";
+// export const API = "http://localhost:3005";
 
 
